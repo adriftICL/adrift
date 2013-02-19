7f20801d9d8c7a41e800dcb03ffd8b2833c89334
Jack Murray <jack@murray.cx>
2013-02-20 10:24:07 +1100

    add messages to setup.sh

--- setup.sh
+++ setup.sh
@@ -10,3 +10,6 @@ which python
 which pip
 pwd
 pip install -r requirements.txt
+
+echo -e "The environment has been created and is up to date."
+echo -e "Use it by sourcing env/bin/activate (type \033[34;1msource $(pwd)/env/bin/activate\033[0m)"
