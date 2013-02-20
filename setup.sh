9386ed8849a1d4a776e54c02db9cf1abf4aaa80c
Jack Murray <jack@murray.cx>
2013-02-20 11:16:24 +1100

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,13 @@ then
     virtualenv env || virtualenv2 env
 fi
 . env/bin/activate
-which python
-which pip
-pwd
-pip install -r requirements.txt
-
-echo -e "The environment has been created and is up to date."
-echo -e "Use it by sourcing env/bin/activate (type \033[34;1msource $(pwd)/env/bin/activate\033[0m)"
+pip install numpy
+if pip install -r requirements.txt
+then
+    echo -e "The environment has been created and is up to date."
+    echo -e "Use it by sourcing env/bin/activate (type \033[34;1msource $(pwd)/env/bin/activate\033[0m)"
+else
+    echo -e "If there were errors installing scipy, please ensure libatlas or blas + lapack are installed."
+    echo -e "On arch linux, this is \033[34;1msudo pacman -S blas lapack\033[0m"
+    # TODO: add ubuntu instructions then add both to this script.
+fi
