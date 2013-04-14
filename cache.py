f0284af923f0930d76e64c648b48e6bfc5443d26
Jack Murray <ktofu@cse.unsw.edu.au>
2013-04-15 09:40:51 +1000

    updated cache to use /mnt

--- cache.py
+++ cache.py
@@ -6,11 +6,14 @@ from os import utime
 import subprocess
 from sys import argv
 
+## Will raise NotCached if not readable/writable (I think..)
+CACHE_ROOT = "/mnt/cached_requests"
+
 class NotCached(Exception):
     pass
 
 def get_filename(closest_index):
-    return 'SavedReqs/closest_index' + str(closest_index).zfill(5)
+    return CACHE_ROOT + '/closest_index' + str(closest_index).zfill(5)
 
 def get_cached_results(closest_index):
     try:
@@ -24,9 +27,10 @@ def cache_results(closest_index, results):
     pickle.dump(results, open(get_filename(closest_index), "wb"))
     subprocess.call(['bash','./delete_stale_saved_reqs.sh'])
 
-# This takes around 20 GB, zipped
-# if __name__ == "__main__":
-#    print "populating cache"
-#    for closest_index in xrange(165 * 360):
-#        print "  |--> processing closest index #" + str(closest_index)
-#        cache_results(closest_index, run_tracer(closest_index))
+# This takes around 20 GB zipped, 150 GB not zipped
+# TODO: compress cache
+if __name__ == "__main__":
+    print "populating cache"
+    for closest_index in xrange(165 * 360):
+        print "  |--> processing closest index #" + str(closest_index)
+        cache_results(closest_index, run_tracer(closest_index))
