004e0400403ba13fd9f6b6fb0af683709e19855c
Erik van Sebille <erik@ccrc178.ccrc.unsw.edu.au>
2013-04-17 13:55:57 +1000

    Added DeepWater Horizon oil spill

--- jetsamina.py
+++ jetsamina.py
@@ -25,6 +25,9 @@ logger.setLevel(INFO)
 @get('/fukushima')
 def map(): return haml(lat=37.8, lng=142, centre=141.0, icon_filename="MarkerTsunami.png")
 
+@get('/deepwaterhorizon')
+def map(): return haml(lat=28, lng=-89.4, centre=-70.0, icon_filename="MarkerOilRig.png")
+
 @get('/sydney')
 def map(): return haml(lat=-33.8, lng=151.2, centre=151.2, icon_filename="MarkerDuckie.png")
 
