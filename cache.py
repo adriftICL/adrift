2f5ee2cf37981889a4aae34194194183915b104c
Erik van Sebille <erik@ccrc178.ccrc.unsw.edu.au>
2013-07-26 16:33:37 +1000

    changed cache.py to save all possible points

--- cache.py
+++ cache.py
@@ -11,7 +11,6 @@ from bz2 import BZ2File
 
 ## Will raise NotCached if not readable/writable (I think..)
 CACHE_ROOT = "cached_requests"
-NUMTOSAVE = "40001"
 
 # open_func = BZ2File
 open_func = open
@@ -39,18 +38,14 @@ def cache_results(closest_index, results):
           pickle.dump(results, handle)
     except:
         raise NotWritten()
-    subprocess.call(['bash','./delete_stale_saved_reqs.sh', CACHE_ROOT, NUMTOSAVE])
 
-# This takes around 20 GB zipped, 150 GB not zipped
-# TODO: compress cache
+# Caching all data
 if __name__ == "__main__":
-    print "populating cache with 15k random entries"
+    print "populating cache with all entries"
     entries = []
     for closest_index in xrange(165 * 360):
         if not (is_landpoint(closest_index) or is_lacking_data(closest_index)):
             entries.append(closest_index)
-    shuffle(entries)
-    entries = entries[:40000] # only 40k entries...
     for closest_index in entries:
         print "  |--> processing closest index #" + str(closest_index)
         cache_results(closest_index, run_tracer(closest_index))
